158ae8a0f38ee2925156712d187a4728427ff829
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-02-11 09:15:12 -0500

    views.py

--- microUsers/users/views.py
+++ microUsers/users/views.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from users.controllers.user_controller import user_controller
 from db.db import db
 from flask_cors import CORS
-from users.controllers.product_controller import product_controller
+#from users.controllers.product_controller import product_controller
 
 app = Flask(__name__)
 CORS(app)
@@ -16,7 +16,7 @@ with app.app_context():
 
 # Registrando el blueprint del controlador de usuarios
 app.register_blueprint(user_controller)
-app.register_blueprint(product_controller)
+#app.register_blueprint(product_controller)
 
 @app.route('/healthcheck')
 def health_check():
