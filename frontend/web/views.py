cee91f8aa322baaddc18c9493a2738b02e30e66b
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-02-11 08:24:36 -0500

    views.py

---
+++ frontend/web/views.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config.from_object('config.Config')
+
+# Ruta para renderizar el template index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+# Ruta para renderizar el template users.html
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+@app.route('/editUser/<string:id>')
+def edit_user(id):
+    print("id recibido",id)
+    return render_template('editUser.html', id=id)
+
+
+@app.route('/editP/<string:id>')
+def edit_product(id):
+    print("code recibido",id)
+    return render_template('editP.html', id=id)
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+
+
+#CONSUL
+
+@app.route('/healthcheck')
+def health_check():
+    return '', 200  # Responde con código 200 si el servicio está funcionando
+
+
+if __name__ == '__main__':
+    app.run()
