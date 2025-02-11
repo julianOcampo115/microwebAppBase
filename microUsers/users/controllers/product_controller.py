66b343537ad9ecc8c8432f023faa70b44ee8e375
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-02-11 08:36:30 -0500

    product_controller.py

---
+++ microUsers/users/controllers/product_controller.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request, jsonify
+from users.models.product_model import Products
+from db.db import db
+
+product_controller = Blueprint('product_controller', __name__)
+
+@product_controller.route('/api/products', methods=['GET'])
+def get_products():
+    print("listado de productos")
+    products = Products.query.all()
+    result = [{'code':product.code, 'name': product.name, 'quantity': product.quantity, 'cost': product.cost, 'bag': product.bag } for product in products]
+    return jsonify(result)
+
+# Get single user by id
+@product_controller.route('/api/products/<int:product_code>', methods=['GET'])
+def get_product(product_code):
+    print("obteniendo producto")
+    product = Products.query.get_or_404(product_code)
+    return jsonify({'code':product.code, 'name': product.name, 'quantity': product.quantity, 'cost': product.cost, 'bag': product.bag})
+
+@product_controller.route('/api/products', methods=['POST'])
+def create_product():
+    print("creando producto")
+    data = request.json
+    #new_user = Users(name="oscar", email="oscar@gmail", username="omondragon", password="123")
+    new_product = Products(name=data['name'], quantity=data['quantity'], cost=data['cost'], bag=data['bag'])
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({'message': 'Product created successfully'}), 201
+
+# Update an existing user
+@product_controller.route('/api/products/<int:product_code>', methods=['PUT'])
+def update_product(product_code):
+    print("actualizando producto")
+    product = Products.query.get_or_404(product_code)
+    data = request.json
+    product.name = data['name']
+    product.quantity = data['quantity']
+    product.cost = data['cost']
+    product.bag = data['bag']
+    db.session.commit()
+    return jsonify({'message': 'Product updated successfully'})
+
+# Delete an existing user
+@product_controller.route('/api/products/<int:product_code>', methods=['DELETE'])
+def delete_product(product_code):
+    product = Products.query.get_or_404(product_code)
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({'message': 'Product deleted successfully'})
