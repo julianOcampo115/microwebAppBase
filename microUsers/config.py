cf53028da5eb92ad8798a6417374915eaa5a9aba
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-02-24 22:41:19 -0500

    config.py

--- microUsers/config.py
+++ microUsers/config.py
@@ -5,3 +5,15 @@ class Config:
     MYSQL_DB = 'myflaskapp'
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+
+
+class Config:
+    MYSQL_HOST = 'db'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'myflaskapp'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
