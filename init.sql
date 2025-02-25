4663841bfdd2ceb1902f72bdc3c4d8d78006b21d
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-02-24 22:45:17 -0500

    init.sql

--- init.sql
+++ init.sql
@@ -23,3 +23,34 @@ INSERT INTO users VALUES(null, "juan", "juan@gmail.com", "juan", "123"),
 
 INSERT INTO products VALUES(null, "Tomates", "4", "500", "yes"),
     (null, "Piñas", "7", "2000", "no");
+
+
+"WHEN YOU WANT TO USE THE DOCKER COMPOSE, AND YOU'VE ALREADY PACKED THIS APP, AND THE DATABASE DOESNT APPEAR, USE THIS INSTEAD:"
+
+CREATE DATABASE IF NOT EXISTS myflaskapp;
+use myflaskapp;
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255),
+    email varchar(255),
+    username varchar(255),
+    password varchar(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS products (
+    code int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255),
+    quantity varchar(255),
+    cost varchar(255),
+    bag varchar(255)
+);
+
+INSERT IGNORE INTO users VALUES
+    (null, "juan", "juan@gmail.com", "juan", "123"),
+    (null, "maria", "maria@gmail.com", "maria", "456");
+
+INSERT IGNORE INTO products VALUES
+    (null, "Tomates", "4", "500", "yes"),
+    (null, "Piñas", "7", "2000", "no");
