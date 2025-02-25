b37f2604075adc775b425e2c1f3413020f0b662c
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-02-24 22:39:11 -0500

    config.py

--- microProducts/config.py
+++ microProducts/config.py
@@ -5,3 +5,14 @@ class Config:
     MYSQL_DB = 'myflaskapp'
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# If you're going to use the docker compose for packing the API REST APP, change this file for:
+
+class Config:
+    MYSQL_HOST = 'db'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'myflaskapp'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
