28094ac0f015f3803fd67bd61d2fe9b44147aa8d
julianOcampo115 <158863132+julianOcampo115@users.noreply.github.com>
2025-03-03 22:28:25 -0500

    product_model.py

--- microProducts/products/models/product_model.py
+++ microProducts/products/models/product_model.py
@@ -1,11 +1,14 @@
 from db.db import db
 
 class Products(db.Model):
+    __tablename__ = 'products'
+
     code = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.String(100), unique=True, nullable=False)
-    cost = db.Column(db.String(100), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    cost = db.Column(db.Float, nullable=False)
     bag = db.Column(db.String(100), nullable=False)
+
 
     def __init__(self, name, quantity, cost, bag):
         self.name = name
